2bd8c262f55c7253e47f1376fde55aff0825cde8
danielhellgren <daniel__hellgren@hotmail.com>
2025-12-01 19:42:37 +0100

    part 2

---
+++ Day1_SecretEntrance/SolutionPart2.py
@@ -0,0 +1,37 @@
+def getPassword(turns):
+    DIAL_INIT = 50
+    dialValue = DIAL_INIT
+    completedRotations = 0
+
+    for turn in turns:
+        completedRotations, dialValue = turnDial(completedRotations, dialValue, turn.replace("\n", ""))
+        
+    print(completedRotations)
+
+def turnDial(completedRotations, dialValue, turn):
+    DIAL_SIZE = 100
+    isLeftTurn = turn[0] == "L"
+    rotations = 0
+    turnValue = int(turn[1:])
+    rotations, turnValue = divmod(turnValue, DIAL_SIZE)
+
+    if(isLeftTurn):
+        turnValue = -turnValue
+
+    newDialValue = (dialValue + turnValue)
+    passedZeroNotStartedFromZero = dialValue != 0 and (newDialValue <= 0 or newDialValue >= DIAL_SIZE)
+
+    if(passedZeroNotStartedFromZero):
+        rotations += 1
+
+    newDialValue = newDialValue % DIAL_SIZE
+    completedRotations += rotations
+
+    return (completedRotations, newDialValue)
+
+def main():
+    file = open("Day1_SecretEntrance\\input.txt", "r")
+    getPassword(file)
+
+if __name__ == "__main__":
+    main()
